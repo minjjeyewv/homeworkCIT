86749428f73c088ea16e5c2f4a6c7fceb4bc1294
minjjeyewv <90327864+minjjeyewv@users.noreply.github.com>
2022-03-10 21:00:54 +0300

    Update helloWorld.cpp
    Добавил функцию фибаначи, первые элементы(1, 1)

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 using namespace std;
 
-
+void fib(int n){
+        int a = 1; int b = 1;
+        for (int i = 0; i < n; i++){
+ cout << a << endl;
+                int c = b;
+ b = a + b;
+ a = c;
+        }
+}
 int main() {
- cout<< "Hello World!"<;
+ cout<< "Hello World!";
 }
