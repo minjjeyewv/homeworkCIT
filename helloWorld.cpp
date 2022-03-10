62de650ba8e56d53651a7c1a6f7b95b805fe1073
minjjeyewv <90327864+minjjeyewv@users.noreply.github.com>
2022-03-10 21:04:39 +0300

    Update helloWorld.cpp
    Программа выводит все числа фибаначи

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void fib(int n){
         int a = 0; int b = 1;
         for (int i = 0; i < n; i++){
- cout << a << endl;
+ cout << a << endl; // вывод всех чисел фибаначи
                 int c = b;
  b = a + b;
  a = c;
